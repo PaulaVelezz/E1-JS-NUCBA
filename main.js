1111c3734a8354429834805c1044dfda860fdead
PaulaVelezz <velezpaula.a@gmail.com>
2022-11-16 16:13:43 -0300

    trabajando en la D

--- main.js
+++ main.js
@@ -1,45 +1,134 @@
-const pizzas = [
+const Pizzas = [
     {
-        nombre: "Margarita",
-        ingredientes: "Salsa Roja, Mozzarella, Albaca Fresca, Aceitunas",
-        precio: "$100",
+        nombre: "Pizza Margarita",
+        ingredientes: 
+        [   
+            "Prepizza",
+            "Salsa Roja", 
+            "Mozzarella",
+            "Albaca Fresca", 
+            "Aceitunas",
+        ],
+        precio: 350,
         id: 1,
     },
     {
-        nombre: "Fugazza",
-        ingredientes: "Cebolla , Parmesano, Aceitunas",
-        precio: "$150pe",
+        nombre: "Pizza Fugazza",
+        ingredientes: 
+        [   
+            "Prepizza",
+            "Cebolla", 
+            "Parmesano",
+            "Aceitunas",
+        ],
+        precio: 400,
         id: 2,
     },
     {
-        nombre: "Fugazzeta",
-        ingredientes: "Cebolla, Mozzarella, Salsa Roja, Aceitunas",
-        precio: "$180pe",
+        nombre: "Pizza Fugazzeta",
+        ingredientes:
+        [   
+            "Prepizza",
+            "Salsa Roja",
+            "Cebolla", 
+            "Parmesano",
+            "Mozzarella",
+            "Aceitunas",
+        ],
+        precio: 450,
         id: 3,
     },
     {
-        nombre: "Pasto",
-        ingredientes: "Mozzarella, Salsa Roja, Pesto, Aceitunas",
-        precio: "$100pe",
+        nombre: "Pizza Pasto",
+        ingredientes: 
+        [   
+            "Prepizza",
+            "Salsa Roja",
+            "Pesto", 
+            "Aceite de oliva",
+            "Mozzarella",
+            "Aceitunas",
+        ],
+        precio: 400,
         id: 4,
     },
     {
-        nombre: "Palmitos",
-        ingredientes: "Palmitos, Mozzarella, Salsa Golf, Aceitunas",
-        precio: "$200pe",
+        nombre: "Pizza Palmitos",
+        ingredientes: 
+        [   
+            "Prepizza",
+            "Salsa Roja",
+            "Salsa Golf", 
+            "Palmitos",
+            "Mozzarella",
+            "Aceitunas",
+        ],
+        precio: 600,
         id: 5,
     },
     {
-        nombre: "Hawaiana",
-        ingredientes: "Anana, Cerezas, Jamón, Mozzarella",
-        precio: "$250pe",
+        nombre: "Pizza Hawaiana",
+        ingredientes: 
+        [   
+            "Prepizza",
+            "Salsa Roja",
+            "Anana", 
+            "Cerezas",
+            "Mozzarella",
+            "Jamón",
+            "Aceitunas",
+        ],
+        precio: 650,
         id: 6,
     }
 ]
 
-/*a)  Las pizzas que tengan un id impar. b) ¿Hay alguna pizza que valga menos de $600? c) El nombre de cada pizza con su respectivo precio. 
-d) Todos los ingredientes de cada pizza  (En cada iteración imprimir los ingredientes de la pizza actual). 
-Ayuda: van a tener que realizar dos recorridos, ya que cada pizza del array de pizzas tiene un array de ingredientes.
+// a) imprimir en consola las pizzas que tengan un id impar.
 
-Un ejemplo de lo que sería una repuesta "user-friendly": 
-"La pizza X, tiene un valor de $XXXX”.*/ 
+const idImpar = Pizzas.filter( (pizza) => {
+    return pizza.id % 2 !== 0;
+});
+
+console.log(idImpar);
+
+idImpar.forEach( (pizza)=> {
+    console.log(`La ${pizza.nombre} tienen un id impar`);
+});
+
+//b) ¿Hay alguna pizza que valga menos de $600?
+
+const pizzaMinPrice = Pizzas.find( (pizza) => {
+     return pizza.precio < 600
+});
+
+console.log(pizzaMinPrice);
+   
+//c) El nombre de cada pizza con su respectivo precio. 
+
+const pizzaInfoData = Pizzas.map( (pizza) => {
+    return {
+      nombre: pizza.nombre,
+      precio: pizza.precio
+    }
+  });
+
+  console.log(pizzaInfoData);
+
+  pizzaInfoData.forEach( (pizza)=> {
+    console.log(`La ${pizza.nombre} cuesta $ ${pizza.precio}`);
+});
+
+//d) Todos los ingredientes de cada pizza.(En cada iteración imprimir los ingredientes de la pizza actual). 
+
+const pizzaIngredientesData= Pizzas.map( (pizza) => {
+    return {
+      nombre: pizza.nombre,
+      ingrediente: pizza.ingredientes
+    }
+  });
+
+  console.log(pizzaIngredientesData);
+
+  pizzaIngredientesData.forEach( (pizza)=> {
+    console.log(`Los ingredientes de ${pizza.nombre} son los siguentes: ${pizza.ingredientes}`);
+});
